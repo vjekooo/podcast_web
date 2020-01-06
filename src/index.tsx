8354918fc42bab4352f911c80db207bc28f3bc46
Vjeko <vjeko.pavicin@gmail.com>
2020-01-06 10:35:11 +0100

    ..

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,8 @@ import App from './App'
 import { getAccessToken } from './accessToken'
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql',
+	// uri: 'http://localhost:4000/graphql',
+	uri: 'ec2-63-32-44-201.eu-west-1.compute.amazonaws.com:4000/graphql',
 	credentials: 'include',
 	request: (operation): void => {
 		const accessToken = getAccessToken()
