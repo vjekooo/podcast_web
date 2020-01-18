dd2503dd5e42990f7bd99d5d304a4e66535489ad
Vjeko <vjeko.pavicin@gmail.com>
2020-01-18 16:58:38 +0100

    ..

--- src/helpers.ts
+++ src/helpers.ts
@@ -34,7 +34,7 @@ interface TokenResponse {
 
 export const refreshToken = (): Promise<TokenResponse> => {
 	return new Promise((resolve, reject) => {
-		const data = window.fetch('http://localhost:4000/refresh_token', {
+		const data = window.fetch('http://0.0.0.0:4000/refresh_token', {
 			method: 'POST',
 			credentials: 'include'
 		})
