4216b5ef2ea0f9ff8fddab9b64cc55d99c13e015
Vjeko <vjeko.pavicin@gmail.com>
2020-05-05 21:19:30 +0200

    cypress config and test

---
+++ cypress/integration/signup_login.spec.js
@@ -0,0 +1,17 @@
+// Register and login
+it('signup and login user', () => {
+	cy.visit('http://localhost:3000/register')
+
+	cy.get('input[name="email"]').type('vjeko.pavicin@gmail.com')
+	cy.get('input[name="password"]').type('test1234')
+	cy.get('button').click()
+	cy.get('button').click()
+
+	cy.location('pathname').should('eq', '/')
+
+	cy.visit('http://localhost:3000/login')
+
+	cy.login('vjeko.pavicin@gmail.com', 'test1234')
+
+	cy.location('pathname').should('eq', '/')
+})
