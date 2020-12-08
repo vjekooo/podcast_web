cba3921f830f6ae66bb4704bf14259568bbe0909
Vjeko <vjeko.pavicin@gmail.com>
2020-12-08 18:51:07 +0100

    start

---
+++ infra/provider.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 0.12"
+}
+provider "aws" {
+  version = "~> 2.12"
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
