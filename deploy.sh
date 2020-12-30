f514ed7ccc56e94faf302ff7c43d5aac9e60a06d
Vjeko <vjeko.pavicin@gmail.com>
2020-12-30 23:54:37 +0100

    infra

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 150363524457.dkr.ecr.eu-west-1.amazonaws.com
-
-docker push 150363524457.dkr.ecr.eu-west-1.amazonaws.com/podcast_web:latest
